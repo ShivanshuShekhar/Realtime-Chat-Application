de60cafc587c3299cbbc2cd7ac25d90be56bcbef
Shivanshu Shekhar <shivanshushekhar786@gmail.com>
2023-10-29 20:17:28 +0530

    Update ChannelListContainer.jsx

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -4,6 +4,7 @@ import Cookies from 'universal-cookie';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import HospitalIcon from '../assets/hospital.png';
+import LogoutIcon from '../assets/logout.png';
 
 const SideBar = () => (
   <div className='channel-list__sidebar'>
@@ -12,12 +13,28 @@ const SideBar = () => (
           <img src={HospitalIcon} alt="Hospital" width='30' />
       </div>
     </div>
+    <div className='channel-list__sidebar__icon1'>
+      <div className='icon1__inner'>
+          <img src={LogoutIcon} alt="Logout" width='30' />
+      </div>
+    </div>
+  </div>
+);
+
+const CompanyHeader = () => (
+  <div className='channel-list__header'>
+    <p className='channel-list__header__text'>Medical Pager</p>
   </div>
 )
 
 const ChannelListContainer = () => {
   return (
-    <div>ChannelListContainer</div>
+    <>
+      <SideBar />
+      <div className='channel-list__list__wrapper'>
+        <CompanyHeader />
+      </div>
+    </>
   );
 }
 
